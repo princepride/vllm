27f22f645d926507e5b27909dd2f6e7c9bcb478d
汪志鹏 <wangzhipeng628@gmail.com>
2025-06-06 19:52:43 +0800

    init\
    Signed-off-by: 汪志鹏 <wangzhipeng628@gmail.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -220,6 +220,7 @@ _MULTIMODAL_MODELS = {
     "Llama4ForConditionalGeneration": ("mllama4", "Llama4ForConditionalGeneration"),  # noqa: E501
     "SkyworkR1VChatModel": ("skyworkr1v", "SkyworkR1VChatModel"),
     "WhisperForConditionalGeneration": ("whisper", "WhisperForConditionalGeneration"),  # noqa: E501
+    "DonutModel": ("donut", "DonutModel")
 }
 
 _SPECULATIVE_DECODING_MODELS = {
