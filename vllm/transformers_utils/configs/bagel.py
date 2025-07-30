b84ab4d10b953bed635d70e64c322f83ce9f85fc
汪志鹏 <wangzhipeng628@gmail.com>
2025-07-30 15:07:13 +0800

    xxx

---
+++ vllm/transformers_utils/configs/bagel.py
@@ -0,0 +1,31 @@
+from transformers import PretrainedConfig
+
+class BagelConfig(PretrainedConfig):
+    model_type = "bagel"
+    def __init__(
+        self,
+        visual_gen=True,
+        visual_und=True,
+        llm_config=None,
+        vit_config=None,
+        vae_config=None,
+        latent_patch_size=2,
+        max_latent_size=32,
+        vit_max_num_patch_per_side=70,
+        connector_act="gelu_pytorch_tanh",
+        interpolate_pos=False,
+        timestep_shift=1.0,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.visual_gen = visual_gen
+        self.visual_und = visual_und
+        self.llm_config = llm_config
+        self.vit_config = vit_config
+        self.vae_config = vae_config
+        self.latent_patch_size = latent_patch_size
+        self.max_latent_size = max_latent_size
+        self.vit_max_num_patch_per_side = vit_max_num_patch_per_side
+        self.connector_act = connector_act
+        self.interpolate_pos = interpolate_pos
+        self.timestep_shift = timestep_shift
